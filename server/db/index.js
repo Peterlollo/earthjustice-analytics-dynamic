c3d80571ea55f56008697a35bd343d039cb08500
Peter Lollo <peteralollo@gmail.com>
2018-10-28 15:33:53 -0700

    Rm commented code from db startup file

--- server/db/index.js
+++ server/db/index.js
@@ -5,11 +5,6 @@ const pool = new Pool({
   connectionString: connectionString,
 })
 
-// pool.query('SELECT NOW()', (err, res) => {
-//   console.log(err, res)
-//   pool.end()
-// })
-
 const client = new Client({
   connectionString: connectionString,
 })
@@ -20,26 +15,8 @@ const query = (text, params, callback) => {
    return pool.query(text, params, callback)
 }
 
-// const text = `INSERT INTO "watchlist"(provider) VALUES ('abbot laboratories')`
-
-// callback
 // client.query('CREATE TABLE IF NOT EXISTS watchlist (provider varchar(450) NOT NULL)')
 // client.query('CREATE TABLE IF NOT EXISTS whitelist (provider varchar(450) NOT NULL, sector varchar(450) NOT NULL)')
-// client.query('SELECT * from watchlist', null, (err, res) => {
-// 	if (err) {
-//     console.log(err.stack)
-//   } else {
-//     console.log(res.rows[0])
-//   }
-// })
-// client.query(text, null, (err, res) => {
-//   if (err) {
-//     console.log(err.stack)
-//   } else {
-//     console.log(res.rows[0])
-//     // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
-//   }
-// })
 
 // client.end()
 

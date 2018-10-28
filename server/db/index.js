39e71fb17003776006938c5c0f89bb423836cd80
Peter Lollo <peteralollo@gmail.com>
2018-10-28 15:34:26 -0700

    Add create table commands

--- server/db/index.js
+++ server/db/index.js
@@ -15,8 +15,8 @@ const query = (text, params, callback) => {
    return pool.query(text, params, callback)
 }
 
-// client.query('CREATE TABLE IF NOT EXISTS watchlist (provider varchar(450) NOT NULL)')
-// client.query('CREATE TABLE IF NOT EXISTS whitelist (provider varchar(450) NOT NULL, sector varchar(450) NOT NULL)')
+client.query('CREATE TABLE IF NOT EXISTS watchlist (provider varchar(450) NOT NULL)')
+client.query('CREATE TABLE IF NOT EXISTS whitelist (provider varchar(450) NOT NULL, sector varchar(450) NOT NULL)')
 
 // client.end()
 
